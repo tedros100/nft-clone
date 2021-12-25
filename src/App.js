385852c96bba747b3d8f403f697a386e7fc2c73c
tedros <teddykiros123@gmail.com>
2021-12-25 14:35:55 -0800

    last commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import './App.css';
-import Header from './components/Header';
-import CollectionCard from './components/CollectionCard';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Punklist from "./components/Punklist";
+import Main from "./components/Main";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get("https://testnets-api.opensea.io/assets?asset_contract_address=0x266b3202e0565B4dE36201981770200c7BBE385e&order_direction=asc");
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      <CollectionCard/>
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <Punklist punkListData={punkListData} setSelectedPunk={setSelectedPunk} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
 import punkLogo from "../assets/header/cryptopunk-logo.png";
 import searchIcon from "../assets/header/search.png";
 import themeSwitchIcon from "../assets/header/theme-switch.png";
-
-
 export const Header = () => {
   return (
     <div className="header">
@@ -14,7 +12,7 @@ export const Header = () => {
 
       <div className="searchBar">
         <div className="searchIconContainer">
-          <img src={searchIcon}  alt="" />
+          <img src={searchIcon} />
         </div>
         <input className="searchInput" placeholder="Collection, item or user..." />
       </div>
@@ -27,7 +25,7 @@ export const Header = () => {
 
       <div className="headerActions">
         <div className="themeSwitchContainer">
-          <img src={themeSwitchIcon}   alt=""/>
+          <img src={themeSwitchIcon} />
         </div>
       </div>
 
